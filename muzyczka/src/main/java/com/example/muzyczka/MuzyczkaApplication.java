ec52c5e7c897cd2f3249794e8dade9505426e188
CWAN1ACZEK <64434257+CWAN1ACZEK@users.noreply.github.com>
2020-06-02 18:30:49 +0200

    Add files via upload

---
+++ muzyczka/src/main/java/com/example/muzyczka/MuzyczkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.muzyczka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzyczkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuzyczkaApplication.class, args);
+    }
+
+}
---
+++ muzyczka/src/test/java/com/example/muzyczka/MuzyczkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muzyczka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzyczkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
